02962e7d4e19d0dcee89fed28cc85d24684ce9f6
zkj214 <pythonexercise214@gmail.com>
2025-07-23 19:24:36 +0800

    add allowed hosts

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["shoppers-cy7n.onrender.com","www.shoppers-cy7n.onrender.com"]
 
 
 # Application definition
