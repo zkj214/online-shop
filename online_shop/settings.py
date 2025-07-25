4cc362262daaab6552857ea43241df74bed5d52e
zkj214 <pythonexercise214@gmail.com>
2025-07-26 03:35:17 +0800

    update custom domain

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -147,12 +147,12 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media' #don't include in render environment variables
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media' #when retrieving default media files
 
 AWS_S3_FILE_OVERWRITE = False  # Prevents overwriting files with the same name
 AWS_DEFAULT_ACL = None #required
 
-    # Media Files
+    # when storing Media Files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
